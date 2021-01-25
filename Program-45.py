587443aacf216f7f38881f973caddb2e062f6194
Tharani Vadde <tharanivadde247@gmail.com>
2021-01-25 11:49:32 +0530

    Delete Program-45.py

--- Program-45.py
+++
@@ -1,9 +0,0 @@
-# 45.python script to print sum of n  number using recursive function
-def sum(n):  
-   if n == 1:  
-       return n  
-   else:  
-       return n+sum(n-1)  
-# take input from the user  
-num = int(input("Enter a number: "))  
-print(sum(num)) 
