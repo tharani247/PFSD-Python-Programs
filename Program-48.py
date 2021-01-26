5fbd1bdae476f4006d2ea7460e7e7b643a93aa39
Tharani Vadde <tharanivadde247@gmail.com>
2021-01-26 12:37:51 +0530

    Delete Program-48.py

--- Program-48.py
+++
@@ -1,6 +0,0 @@
-#48.Python script to print frequency of every element in given list
-import collections
-my_list = [10,10,10,10,20,20,20,20,40,40,50,50,30]
-print("Original List : ",my_list)
-ctr = collections.Counter(my_list)
-print("Frequency of the elements in the List : ",ctr)
