06e5a48db2807ee6f3fec289b079f7913282a4f4
Tharani Vadde <tharanivadde247@gmail.com>
2021-01-28 19:06:57 +0530

    Delete Program-54.py

--- Program-54.py
+++
@@ -1,8 +0,0 @@
-# 54.python program to demonstrate parameterized constructor
-class Employee:
-    def __init__(e,id,name):
-        print("parameterized constructor")
-        print("Employee id",id,"Employee name",name)
-        
-emp=Employee(19, "hello")#to create an object
-
