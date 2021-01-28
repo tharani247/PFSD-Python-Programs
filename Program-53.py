18c2b8f378701ab2e34d500a1b5dff62b14e5ec8
Tharani Vadde <tharanivadde247@gmail.com>
2021-01-28 19:06:40 +0530

    Delete Program-53.py

--- Program-53.py
+++
@@ -1,5 +0,0 @@
-# 53.python program to demonstrate non parameterized constructor
-class Employee:
-    def __init__(e):
-        print("non parameterized constructor")
-emp=Employee()#to create an object
