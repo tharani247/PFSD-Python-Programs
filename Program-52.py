c67f39ddac995c54a3561db6f30d5481790bfb5c
Tharani Vadde <tharanivadde247@gmail.com>
2021-01-28 19:06:00 +0530

    Delete Program-52.py

--- Program-52.py
+++
@@ -1,9 +0,0 @@
-# 52.python program to demonstrate class and object using __init method
-class Employee:
-    def __init__(e, id, name):
-        e.id=id
-        e.name=name
-    def display(e):
-        print(e.id,e.name)
-emp=Employee(101, "KLU")#to create an object
-emp.display()
